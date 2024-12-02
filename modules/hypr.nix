8d75835ac655bace5a6f7dc240243cd003552401
broot5 <29516943+broot5@users.noreply.github.com>
2024-12-02 14:05:27 +0900

    Update

--- modules/hypr.nix
+++ modules/hypr.nix
@@ -1,6 +1,9 @@
 {
-  programs.hyprland.enable = true;
-  programs.hyprland.withUWSM = true;
+  programs.hyprland = {
+    enable = true;
+    withUWSM = true;
+    xwayland.enable = true;
+  };
 
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
 }
