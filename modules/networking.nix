1f044bdd7adc91e6b191c230e91235b803dfeb1e
broot5 <29516943+broot5@users.noreply.github.com>
2024-12-13 09:46:47 +0900

    SSH

--- modules/networking.nix
+++ modules/networking.nix
@@ -1,6 +1,18 @@
 {
   networking = {
     networkmanager.enable = true;
-    nameservers = [ "1.1.1.1" "1.0.0.1" ];
+    nameservers = [
+      "1.1.1.1"
+      "1.0.0.1"
+    ];
+  };
+
+  services.openssh = {
+    enable = true;
+    settings = {
+      PasswordAuthentication = true;
+      X11Forwarding = false;
+      PermitRootLogin = "no";
+    };
   };
 }
