eacfecb0d98e697712b94baa409ee58ebfbe7ba6
broot5 <29516943+broot5@users.noreply.github.com>
2024-12-16 07:43:21 +0900

    icon theme

--- users/broot/theme.nix
+++ users/broot/theme.nix
@@ -32,11 +32,15 @@
 
   stylix.enable = true;
   stylix.autoEnable = true;
+
   stylix.image = pkgs.fetchurl {
     url = "https://github.com/danihek/dh-wallpapers/blob/main/artwork-anime-landscape-painting-wallpaper-8b962c8d7321cf3955046b39ddaccc80.jpg?raw=true";
     sha256 = "1byqfrx71j0kd6ym14csw05gxni8v8zd68anwgpk36z5ac1cim43";
   };
-  stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/kanagawa.yaml";
+  stylix.polarity = "dark";
+
+  # stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/kanagawa.yaml";
+
   stylix.fonts = {
     serif = config.stylix.fonts.sansSerif;
     sansSerif = {
@@ -47,5 +51,12 @@
       package = pkgs.nerd-fonts.iosevka;
       name = "Iosevka Nerd Font";
     };
+  };
+
+  stylix.iconTheme = {
+    enable = true;
+    package = pkgs.papirus-icon-theme;
+    light = "Papirus-Light";
+    dark = "Papirus-Dark";
   };
 }
